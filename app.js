0f1531f5989422f9515422b4c084a364fd8ef6bb
HoaAyWK <vycoi1511@gmail.com>
2021-08-17 05:56:54 -0700

    Add handling CORS

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Contend-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use('/products', productRoute);
 
 app.use((req, res, next) => {
