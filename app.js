75e403e4407351fb7aba1f86e4b18d4a181376a4
HoaAyWK <vycoi1511@gmail.com>
2021-08-17 04:26:47 -0700

    Add handling errors for app module

--- app.js
+++ app.js
@@ -24,4 +24,13 @@ app.use(morgan('dev'));
 
 app.use('/products', productRoute);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+}, (error, req, res, next) => {
+    res.status(error.status || 500)
+    .json({message: error.message});
+});
+
 module.exports = app;
