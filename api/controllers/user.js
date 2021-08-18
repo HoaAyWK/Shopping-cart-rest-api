34e87c7de54ae60acc02e39a56f54b0904df7e8a
HoaAyWK <vycoi1511@gmail.com>
2021-08-18 07:10:08 -0700

    Add user signin with JWT

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,31 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 const { validation } = require('../validation');
+
+module.exports.postLogin = async (req, res) => {
+    const { error } = validation(req.body);
+    if (error) return res.status(500).json({message: error.details[0].message});
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if (!user) return res.status(404).json({message: 'Auth failed'});
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) return res.status(500).json({message: 'Auth failed'});
+        const token = jwt.sign(
+        {
+            email: user.email,
+            userId: user._id
+        },
+        process.env.JWT_KEY,
+        {
+            expiresIn: '1h'
+        });
+        res.status(200).json({message: 'Auth successful', token: token});
+    } catch(err) {
+        res.status(500).json({error: err.message});
+    }
+};
 
 module.exports.createNewUser = async (req, res) =>  {
     const { error } = validation(req.body);
--- api/routes/user.js
+++ api/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 router.post('/signup', userController.createNewUser);
+router.post('/signin', userController.postLogin);
 router.delete('/:userId', userController.deleteUser);
 
 module.exports = router;
